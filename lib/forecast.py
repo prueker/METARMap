e225da4c3122662292493ed0c902991d29d606aa
Nicolas <njtret0815@gmail.com>
2020-12-18 19:07:06 +0000

    force windspeeds to be integers

--- lib/forecast.py
+++ lib/forecast.py
@@ -76,8 +76,8 @@ def overrideForecastPeriod(period, overrides):
         overrideEndTime = datetime.datetime.strptime(override.find('fcst_time_to').text, '%Y-%m-%dT%H:%M:%SZ')
         overrideVisibility = override.find('visibility_statute_mi') is not None and override.find('visibility_statute_mi').text
         overrideCeiling = findMinimumCeiling(override)
-        overrideWindSpeed = override.find('wind_speed_kt') is not None and override.find('wind_speed_kt').text
-        overrideWindGust = override.find('wind_gust_kt') is not None and override.find('wind_gust_kt').text
+        overrideWindSpeed = override.find('wind_speed_kt') is not None and int(override.find('wind_speed_kt').text)
+        overrideWindGust = override.find('wind_gust_kt') is not None and int(override.find('wind_gust_kt').text)
         overrideLightning = False
         if override.find('raw_text') is not None:
             lightning = False if override.find('raw_text').text.find('LTG') == -1 else True
